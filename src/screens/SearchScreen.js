14daa60a247c0f3d816d249c656662c55c231e93
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-01-17 11:30:37 -0500

    added useEffect for initial search and extracted hook logic out of component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,41 +1,21 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term ,setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [error, setError] = useState("")
-
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term,
-          location: 'san jose',
-        }
-      })
-      setResults(response.data.businesses);
-    } catch (err) {
-      setError("Something went wrong");
-    }
-  }
+  const [searchApi, results, error] = useResults();
 
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={setTerm}
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
       />
       <Text>We have found {results.length} results</Text>
-      {
-        error ?
-        <Text>{error}</Text> :
-        null
-      }
+      { error ? <Text>{error}</Text> : null }
     </View>
   );
 };
