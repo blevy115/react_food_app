2814112021546fb9d018850cabb4cc3a86a0ccfa
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-01-17 02:32:05 -0500

    added axios request for yelp api with error message

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,41 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term ,setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState("")
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose',
+        }
+      })
+      setResults(response.data.businesses);
+    } catch (err) {
+      setError("Something went wrong");
+    }
+  }
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => {console.log('submitted')}}
+        onTermChange={setTerm}
+        onTermSubmit={searchApi}
       />
-      <Text>{term}</Text>
+      <Text>We have found {results.length} results</Text>
+      {
+        error ?
+        <Text>{error}</Text> :
+        null
+      }
     </View>
   );
 };
